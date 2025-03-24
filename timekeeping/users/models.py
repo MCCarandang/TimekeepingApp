854c3d694e733550e3ce8a9fcfd47c6feecba3d9
MCCarandang <carandangmariane19@gmail.com>
2025-03-24 15:30:17 +0800

    may error pa sa user

--- timekeeping/users/models.py
+++ timekeeping/users/models.py
@@ -1,16 +1,28 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
     # Additional fields can be added here
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='custom_user_set',  # Change this to avoid clashes
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='custom_user_set',  # Change this to avoid clashes
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
     pass
 
 class RFIDTag(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tag_id = models.CharField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"RFID {self.tad_id} - {self.user.name}"
+        return self.tag_id
