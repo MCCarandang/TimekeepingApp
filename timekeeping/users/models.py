810d49f4f3e101037fddb18f57c75e5ef26bb2d5
MCCarandang <carandangmariane19@gmail.com>
2025-03-24 14:15:44 +0800

    May error pa nung magawa ang users app

--- timekeeping/users/models.py
+++ timekeeping/users/models.py
@@ -10,3 +10,7 @@ class RFIDTag(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tag_id = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"RFID {self.tad_id} - {self.user.name}"
--- timekeeping/users/views.py
+++ timekeeping/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def users_view(request):
+    return HttpResponse("User list will be displayed here.")
