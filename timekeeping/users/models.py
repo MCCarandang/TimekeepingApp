222f635bb767398a7917aea18a109defb60a5ca3
MCCarandang <carandangmariane19@gmail.com>
2025-03-24 13:23:40 +0800

    Added users app

--- timekeeping/users/models.py
+++ timekeeping/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    # Additional fields can be added here
+    pass
+
+class RFIDTag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag_id = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
