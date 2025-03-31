ead10e8df95a8774bbc57ae69e0f664fb5750b19
MCCarandang <carandangmariane19@gmail.com>
2025-03-31 13:39:22 +0800

    Updated home.html on attendance template

--- timekeeping/attendance/views.py
+++ timekeeping/attendance/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 from django.http import JsonResponse  # To return JSON responses
 from .models import Attendance      #imports the models.py from attendance app
 from django.utils import timezone
+import pytz
 
 # Create your views here.
 def home(request):
-    current_time = timezone.now()
-    return render(request, 'attendance/home.html', {'current_time': current_time})      #Gets the current date and time using Django's timezone.now(). This ensures time is stored in a timezone-aware format.
+    current_time_utc = timezone.now()
+
+    # Convert to Philippine Time
+    philippine_tz = pytz.timezone('Asia/Manila')
+    current_time = current_time_utc.astimezone(philippine_tz)
+
+    current_date = current_time.date()  # Get the date
+    current_time_only = current_time.time() # Get the time
+    return render(request, 'attendance/home.html', {
+        'current_date': current_date,
+        'current_time': current_time_only
+        })  # Gets the current date and time using Django's timezone.now(). This ensures time is stored in a timezone-aware format.
 
 def check_in_out(user_id):
     last_record = Attendance.objects.filter(user_id=user_id).order_by('timestamp').first()
