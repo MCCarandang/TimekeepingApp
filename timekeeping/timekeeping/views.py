0d819be8bceacfc50fdce6b46a63f3adbc02d9cc
MCCarandang <carandangmariane19@gmail.com>
2025-04-04 07:34:50 +0800

    fixed problem in views.py about the context

--- timekeeping/timekeeping/views.py
+++ timekeeping/timekeeping/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def time_entries_list(request):
+    context = {}    # Define a context dictionary (add your data here)
     return render(request, 'template_name.html', context)
