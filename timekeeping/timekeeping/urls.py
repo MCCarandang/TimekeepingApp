ff5498429796b3aac3a25f37caf0021d6ef5e6f2
MCCarandang <carandangmariane19@gmail.com>
2025-04-04 13:51:55 +0800

    modified urls.py

--- timekeeping/timekeeping/urls.py
+++ timekeeping/timekeeping/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('attendance.urls')),
+    path('access/', include('access_control.urls')),
 ]
