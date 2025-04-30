cb0370992f4d53b6e05e1db8952b4d69114c6274
MCCarandang <carandangmariane19@gmail.com>
2025-04-30 14:39:26 +0800

    updated a lot

--- repeat_scan/repeat_camera.py
+++ repeat_scan/repeat_camera.py
@@ -1,19 +1,20 @@
 # Repeated Transaction Working Fine
 # It shows user name, id number, photo
+# Added exit button
 
 import sys
 import time
+from time import sleep
 import os
 import sqlite3
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from picamera import PiCamera
-from io import BytesIO
+import pygame
+import subprocess
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QPushButton
 from PyQt5.QtGui import QPalette, QColor, QFont, QPixmap
 from PyQt5.QtCore import Qt, QTimer, QDateTime, QByteArray, QBuffer
-
-SPECIAL_RFID_TAG = "529365863836"
 
 def get_label_from_code(code):
     return "IN" if code == 'I' else "OUT"
@@ -106,9 +107,58 @@ class AccessGrantedWindow(QMainWindow):
         # RFID Setup
         GPIO.setwarnings(False)
         self.reader = SimpleMFRC522()
+
+        # Initialize the camera
+        camera = PiCamera()
+
+        # Flip the camera vertically to be not upside down
+        camera.vflip = True
+
+        # Initialize pygame for the camera preview
+        pygame.init()
+
+        # Set the display dimensions for the preview (adjust as needed)
+        SCREEN_WIDTH = 800
+        SCREEN_HEIGHT = 480
+
+        # Adjust camera preview dimensiona for a smaller size
+        CAMERA_PREVIEW_WIDTH = 180
+        CAMERA_PREVIEW_HEIGHT = 180
+
+        # Set the camera preview resolution
+        camera.resolution = (CAMERA_PREVIEW_WIDTH, CAMERA_PREVIEW_HEIGHT)
+
+        # Initialize the camera preview at the lower left corner
+        camera.start_preview(fullscreen=False, window=(0, SCREEN_HEIGHT - CAMERA_PREVIEW_HEIGHT, CAMERA_PREVIEW_WIDTH, CAMERA_PREVIEW_HEIGHT))
+
+        # Wait for a moment to ensure the camera preview window is created
+        sleep(2)
+
+        # Get the process ID (PID) of the camera preview window
+        preview_pid = os.getpid()
+
+        # Hide the GUI window of the camera preview
+        subprocess.run(["xdotool", "search", "--onlyvisible", "--pid", str(preview_pid), "windowunmap"])
+
+        # Define clocking state (0 for clocking in, 1 for clocking out)
+        clocking_state = 0
+
+        # Define the RFID tag UID for changing clocking state
+        clocking_state_serial = "5BBFBE0D"
+
+        # Establish a connection to the database
+        db_connection = sqlite3.connect('/home/raspberrypi/Desktop/Timekeeping_Defective_IDs/attendance_logs/database/DATABASE_TTECH.db', check_same_thread=False)
+        db_cursor = db_connection.cursor()
+
+        # Define the directory of the photos for all registered users
+        registered_users_photo_directory = "/home/raspberrypi/Desktop/Timekeeping_Defective_IDs/attendance_logs/valid_users"
         
-        # State
-        self.current_state = "IN"
+        # Define the directory of the photos for all denied users captured by camera
+        denied_users_photo_directory = "/home/raspberrypi/Desktop/Timekeeping_Defective_IDs/attendance_logs/denied_users"
+
+        # Create a Pygame screen for the camera preview
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Camera Preview")
 
     def update_date_time(self):
         current_time = QDateTime.currentDateTime()
@@ -150,15 +200,6 @@ class AccessGrantedWindow(QMainWindow):
             self.transaction_code_label.setText("IN" or "OUT")
             self.message_label.setText("ACCESS GRANTED")
             self.show_user_info(name, id_number, photo_pixmap)
-            
-    def handle_special_tag(self):
-        #Trigger IN/OUT label without accessing the database
-        self.current_state = "OUT" if self.current_state == "IN" else "IN"
-        self.transaction_code_label.setText(self.current_state)
-        self.message_label.setText("TAP YOUR RFID TAG")
-        self.user_name_label.clear()
-        self.id_number_label.clear()
-        self.photo_label.clear()
 
     def check_rfid(self):
         try:
@@ -166,13 +207,6 @@ class AccessGrantedWindow(QMainWindow):
 
             if id:
                 rfid_str = str(id)
-                
-                # Handles the RFID Tag that triggers only the IN and OUT
-                if rfid_str == SPECIAL_RFID_TAG:
-                    self.handle_special_tag()
-                    QTimer.singleShot(3000, self.reset_ui)
-                    return
-                
                 conn = sqlite3.connect('/home/raspberrypi/Desktop/Timekeeping/timekeepingapp.db')
                 cursor = conn.cursor()
 
@@ -206,29 +240,11 @@ class AccessGrantedWindow(QMainWindow):
                             self.clear_user_info()  # clear user info immediately
                             self.message_label.setText("REPEATED ACTION")
                             self.transaction_code_label.setText("IN")
-                            
-                            # Check if a repeated scan already exists for the same RFID and transaction code
+                
                             cursor.execute("""
-                                SELECT id, scan_count FROM repeated_scans
-                                WHERE rfid_tag = ? AND transaction_code = 'I'
-                                ORDER BY scan_time DESC LIMIT 1
-                            """,(rfid_str,))
-                            existing_scan = cursor.fetchone()
-                            
-                            if existing_scan:
-                                # Update scan_count by incrementing it
-                                scan_id, current_count = existing_scan
-                                cursor.execute("""
-                                    UPDATE repeated_scans
-                                    SET scan_count = ?, scan_time = ?
-                                    WHERE id = ?
-                                """, (current_count + 1, current_time, scan_id))
-                            else:
-                                # Insert new record with scan_count = 1
-                                cursor.execute("""
-                                    INSERT INTO repeated_scans (rfid_tag, transaction_code, scan_time)
-                                    VALUES (?, 'I', ?)
-                                """, (rfid_str, current_time))
+                                INSERT INTO repeated_scans (rfid_tag, transaction_code, scan_time)
+                                VALUES (?, 'I', ?)
+                            """, (rfid_str, current_time))
                 
                             conn.commit()
                             conn.close()
@@ -281,20 +297,19 @@ class AccessGrantedWindow(QMainWindow):
                     """, (rfid_str,))
                     last_denied = cursor.fetchone()
 
-                    #if last_denied:
-                    new_transaction_code = 'O' if last_denied and last_denied[0] == 'I' else 'I'
-                        #cursor.execute(""" 
-                         #   UPDATE denied_usr 
-                          #  SET transaction_code = ?, attempt_time = ? 
-                           # WHERE rfid_tag = ?
-                        #""", (new_transaction_code, current_time, rfid_str))
-                    #else:
-                     #   new_transaction_code = 'I'
-                        
-                    cursor.execute(""" 
-                        INSERT INTO denied_usr (rfid_tag, transaction_code, attempt_time) 
-                        VALUES (?, ?, ?) 
-                    """, (rfid_str, new_transaction_code, current_time))
+                    if last_denied:
+                        new_transaction_code = 'O' if last_denied[0] == 'I' else 'I'
+                        cursor.execute(""" 
+                            UPDATE denied_usr 
+                            SET transaction_code = ?, attempt_time = ? 
+                            WHERE rfid_tag = ?
+                        """, (new_transaction_code, current_time, rfid_str))
+                    else:
+                        new_transaction_code = 'I'
+                        cursor.execute(""" 
+                            INSERT INTO denied_usr (rfid_tag, transaction_code, attempt_time) 
+                            VALUES (?, ?, ?) 
+                        """, (rfid_str, new_transaction_code, current_time))
 
                     self.message_label.setText("ACCESS DENIED")
                     self.transaction_code_label.setText(get_label_from_code(new_transaction_code))
@@ -309,7 +324,7 @@ class AccessGrantedWindow(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            self.showNormal()
+            QApplication.quit()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
